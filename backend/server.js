0a883543789c5b7b268ae54444396d7389cf3cea
leminhtriet3105206 <leminhtriet3105206@gmail.gmailcom>
2026-04-14 16:50:18 +0700

    sửa

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.set('view engine', 'ejs');
